e0165027745b0114ed5c5500acb74d3b19c6081a
ctomera <ctoemra@etu.u-bordeaux.fr>
2023-12-22 09:00:01 +0100

    copy the code of the room in the navbar

--- spotify-web-player/src/Components/NavBar.jsx
+++ spotify-web-player/src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Menu, Header, Icon, Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Menu, Header, Button, Popup } from "semantic-ui-react";
 import { useContext } from "react";
 import { PlayerContext } from "./context/PlayerContext";
 import { useNavigate } from "react-router-dom";
@@ -9,25 +9,46 @@ export default function NavBar() {
   const { room, leaveRoom} = useContext(PlayerContext);
   const navigate = useNavigate();
 
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setPopupOpen(true);
+    setTimeout(() => {
+      setPopupOpen(false);
+    }, 1000);
+  };
+
   const leaveRoomNavigate = () => {
     leaveRoom();
     navigate("/", { replace: true });
   };
 
   return (
-    <Menu>
+    
+    <Menu inverted>
       <Menu.Item>
-        <Header as="h1">Cyclops</Header>
+        <Header as="h1" style={{color:"white"}} >Cyclops</Header>
       </Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item>
-          {room}
-        </Menu.Item>
-        <Menu.Item>
-          <Button color="red" onClick={leaveRoomNavigate}>
-            <Icon name="logout"/> Quitter
-          </Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>);
+      {room && (
+        <Menu.Menu position='right'>
+          <Menu.Item onClick={() => {navigator.clipboard.writeText(room)}}>
+            Code de la partie: 
+            <Popup
+      position='bottom left'
+      content='Code copié !'
+      on='click'
+      open={popupOpen}
+      trigger={
+        <Button style={{marginLeft: "1em"}} icon="copy" content={room} onClick={handleButtonClick} />
+      }
+    />
+          </Menu.Item>
+          <Menu.Item>
+            <Button color="red" icon="sign-out" onClick={leaveRoomNavigate}>
+            </Button>
+          </Menu.Item>
+        </Menu.Menu>
+      )}
+    </Menu>
+  );
 }
