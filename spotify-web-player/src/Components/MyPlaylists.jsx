7ccca70764447d5e7c1f76c13bc799a1ffd661c2
AlexGmb23 <alex.guimbeau@etu.u-bordeaux.fr~>
2024-01-08 08:30:06 +0100

    reprise

--- spotify-web-player/src/Components/MyPlaylists.jsx
+++ spotify-web-player/src/Components/MyPlaylists.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { all } from "axios";
 import Quizz from "./Quizz";
 import { Link } from "react-router-dom";
-import { Button, Image, Grid, Segment, Checkbox } from "semantic-ui-react";
+import { Button, Image, Grid, Segment, Container } from "semantic-ui-react";
 
 function MyPlaylists({ onSelectPlaylist }) {
   const [playlists, setPlaylists] = useState([]);
+  const [allTracks, setAllTracks] = useState([]);
 
   const changeColor = (playlistId) => {
     // Call the onSelectPlaylist callback with the playlist ID
     onSelectPlaylist(playlistId);
     console.log("playlistId", playlistId);
 
-    // change the color of the selected playlist
     const playlist = document.getElementById(playlistId);
     playlist.className = "ui button green fluid";
 
-    // reset the color of the other playlists
     const playlists = document.getElementsByClassName("ui button green fluid");
     for (let i = 0; i < playlists.length; i++) {
       if (playlists[i].id !== playlistId) {
@@ -46,6 +45,38 @@ function MyPlaylists({ onSelectPlaylist }) {
     getMyPlaylists();
   }, []);
 
+  async function getAllTracks() {
+    try {
+      playlists.map((playlist) => {
+        const response = axios.get(
+          `https://api.spotify.com/v1/playlists/${playlist.id}/tracks`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const tracksData = response.data.items;
+        console.log("All tracks:", tracksData);
+        setAllTracks(tracksData);
+      });
+    } catch (error) {
+      console.error("Error retrieving all tracks:", error);
+    }
+  }
+
+  function allTracksNames(playlist) {
+    const allTracks = Promise.all(getAllTracks(playlist));
+    allTracks.then((tracks) => {
+      tracks.map((track) => {
+        track.map((track) => {
+          console.log("track", track.track.name);
+          return <p>{track.track.name}</p>;
+        });
+      });
+    });
+  }
+
   return (
     <div>
       <h1>My Playlists</h1>
@@ -63,11 +94,25 @@ function MyPlaylists({ onSelectPlaylist }) {
                     id={playlist.id}
                   >
                     <h3>{playlist.name}</h3>
-                    <Image
-                      src={playlist.images[0].url}
-                      alt={playlist.name}
-                      size="small"
-                    />
+                    <Grid columns={2}>
+                      <Grid.Column>
+                        <Image
+                          src={playlist.images[0].url}
+                          alt={playlist.name}
+                          size="small"
+                        />
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Container
+                          style={{
+                            overflowY: "scroll",
+                            height: "15vh",
+                          }}
+                        >
+                          {console.log("truc")}
+                        </Container>
+                      </Grid.Column>
+                    </Grid>
                     <p>{playlist.tracks.total} tracks</p>
                     <p>{playlist.description}</p>
                   </Button>
@@ -89,11 +134,25 @@ function MyPlaylists({ onSelectPlaylist }) {
                     id={playlist.id}
                   >
                     <h3>{playlist.name}</h3>
-                    <Image
-                      src={playlist.images[0].url}
-                      alt={playlist.name}
-                      size="small"
-                    />
+                    <Grid columns={2}>
+                      <Grid.Column>
+                        <Image
+                          src={playlist.images[0].url}
+                          alt={playlist.name}
+                          size="small"
+                        />
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Container
+                          style={{
+                            overflowY: "scroll",
+                            height: "15vh",
+                          }}
+                        >
+                          {console.log("truc")}
+                        </Container>
+                      </Grid.Column>
+                    </Grid>
                     <p>{playlist.tracks.total} tracks</p>
                     <p>{playlist.description}</p>
                   </Button>
