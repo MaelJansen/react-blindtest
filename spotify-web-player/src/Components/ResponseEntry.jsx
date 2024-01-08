d7de43dffc1041b70b7184af8e5ab7521111e531
juledupont <jules.dupont.1@etu.u-bordeaux.fr>
2024-01-08 09:19:35 +0100

    merge fix

--- spotify-web-player/src/Components/ResponseEntry.jsx
+++ spotify-web-player/src/Components/ResponseEntry.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Dropdown, Feed } from "semantic-ui-react";
+import { Dropdown } from "semantic-ui-react";
 import axios, { all } from "axios";
 import { TrackContext } from "./SpotifyContext";
 import { SocketContext } from "./context/SocketContext";
@@ -9,8 +9,7 @@ import { v4 as uuidv4 } from "uuid";
 export default function ResponseEntry() {
   const [titles, setTitles] = useState([]);
   const [artistes, setArtistes] = useState([]);
-  const { allTracks } = useContext(TrackContext);
-  const { currentTrack } = useContext(TrackContext);
+  const { allTracks, currentTrack } = useContext(TrackContext);
   const username = localStorage.getItem("username");
   const room = localStorage.getItem("room");
   const [response, setResponse] = useState(false);
@@ -19,26 +18,31 @@ export default function ResponseEntry() {
   const [selectedArtist, setSelectedArtist] = useState("");
   const socket = useContext(SocketContext);
 
-  const submitMessage = (message) => {
-    const __createdtime__ = Date.now();
-    socket.emit("send_message", { message, username, room, __createdtime__ });
-  };
-
   useEffect(() => {
-    const shuffledTitles = shuffleArray(Object.keys(allTracks).map(track => allTracks[track].track.name));
-    const shuffledArtistes = shuffleArray(Object.keys(allTracks).map(track => allTracks[track].track.artists[0].name));
+    const shuffledTitles = shuffleArray(
+      Object.keys(allTracks).map((track) => allTracks[track].track.name)
+    );
+    const shuffledArtistes = shuffleArray(
+      Object.keys(allTracks).map(
+        (track) => allTracks[track].track.artists[0].name
+      )
+    );
 
-    setTitles(shuffledTitles.map(title => ({
-      key: title,
-      text: title,
-      value: title,
-    })));
+    setTitles(
+      shuffledTitles.map((title) => ({
+        key: title,
+        text: title,
+        value: title,
+      }))
+    );
 
-    setArtistes(shuffledArtistes.map(artist => ({
-      key: artist,
-      text: artist,
-      value: artist,
-    })));
+    setArtistes(
+      shuffledArtistes.map((artist) => ({
+        key: artist,
+        text: artist,
+        value: artist,
+      }))
+    );
 
     console.log("tracks: ", allTracks);
   }, [allTracks]);
@@ -52,10 +56,35 @@ export default function ResponseEntry() {
     return newArray;
   }
 
+  const submitMessage = (message) => {
+    const __createdtime__ = Date.now();
+    socket.emit("send_message", { message, username, room, __createdtime__ });
+  };
+
+  function isResponse() {
+    var message = "à gagné 8 points";
+    if (selectedTitle === currentTrack.track.name) {
+      setResponse(true);
+      submitMessage(message);
+    } else {
+      setResponse(false);
+    }
+  }
+
+  function isResponseArtist() {
+    var message = "à gagné 2 points";
+    if (selectedArtist === currentTrack.track.artists[0].name) {
+      setResponseArtist(true);
+      submitMessage(message);
+      console.log("test");
+    } else {
+      setResponseArtist(false);
+    }
+  }
+
   return (
     <div>
       {console.log("title :", titles)}
-      {console.log("current track : ", currentTrack)}
       <Dropdown
         placeholder="Choisissez un tire"
         fluid
