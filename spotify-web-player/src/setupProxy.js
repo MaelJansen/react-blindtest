6788d700beb975464cd9882b585d04875bd617e1
juledupont <jules.dupont.1@etu.u-bordeaux.fr>
2024-01-09 11:02:03 +0100

    helios network

--- spotify-web-player/src/setupProxy.js
+++ spotify-web-player/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use('/auth/**', 
         createProxyMiddleware({ 
-            target: 'http://localhost:5000'
+            target: 'http://192.168.138.93:5000'
         })
     );
 };
