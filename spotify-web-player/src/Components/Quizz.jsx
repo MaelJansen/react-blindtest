b7b4e87e9c8ea68b3e9fdeaafeeceba9f03f6803
AlexGmb23 <alex.guimbeau@etu.u-bordeaux.fr~>
2024-01-10 09:58:35 +0100

    refacto

--- spotify-web-player/src/Components/Quizz.jsx
+++ spotify-web-player/src/Components/Quizz.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import TrackPlayer from "./TrackPlayer";
 import { TrackContext } from "./SpotifyContext";
 import { PlayerContext } from "./context/PlayerContext";
@@ -45,7 +44,6 @@ function Quizz(props) {
   const socket = React.useContext(SocketContext);
 
   const [tracks, setTracks] = useState([]);
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
   const token = localStorage.getItem("token");
   const { allTracks, updateAllTracks } = useContext(TrackContext);
   const { currentTrack, updateCurrentTrack } = useContext(TrackContext);
